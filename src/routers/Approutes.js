2b856d9b0ab2250864fcf2a46e9abe8666aaad19
aryan21710 <aryan21710@gmail.com>
2020-10-19 20:57:13 +0530

    APPROUTES SHOWING LANDINA PAGE

---
+++ src/routers/Approutes.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import PrivateRoute from './PrivateRoute';
+import ErrorBoundary from '../components/common/ErrorBoundaryComp';
+
+// *** LAZY LOAD ALL COMPONENTS STARTS ***
+const LandingPage  = lazy(() => import('../components/LandingPage'));
+
+class DebugRouter extends Router {
+	constructor(props) {
+		super(props);
+		console.log('initial history is: ', JSON.stringify(this.history, null, 2));
+		this.history.listen((location, action) => {
+			console.log(`The current URL is ${location.pathname}`);
+			console.log(
+				`The last navigation action was ${action} with state as ${location.state}`,
+				JSON.stringify(this.history, null, 2)
+			);
+		});
+	}
+}
+
+const Approutes = () => {
+	return (
+		<DebugRouter>
+			<Switch>
+				<Suspense fallback={<div>Loading</div>}>
+					<Route path={`/`} exact={true} strict component={LandingPage} />
+				</Suspense>
+			</Switch>
+		</DebugRouter>
+	);
+};
+
+Approutes.propTypes = {};
+
+export default Approutes;
