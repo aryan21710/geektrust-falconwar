63f0e17599d7e1740d47ad6a72090df6fea56ec6
aryan21710 <aryan21710@gmail.com>
2020-10-28 22:03:34 +0530

    DISPLAY FINAL RESULT IS READY WITH CUSTOM HOOKS

--- src/components/DisplayFinalResult.js
+++ src/components/DisplayFinalResult.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Wrapper, BadgeWrapper, ImageWrapper, ButtonWrapper, Heading } from './common/StyledComponent';
+import React, { useContext, useState, useEffect } from 'react';
+import { SelectedPlanetWrapper, Heading } from './common/StyledComponent';
+import { PlanetDetailsContext } from '../context/appContext';
+import { usePostDataToFetchResult } from '../customHooks/usePostDataToFetchResult';
+
 const DisplayFinalResult = () => {
+	const { finalData } = useContext(PlanetDetailsContext);
+	const [result, setResult] = useState("");
+    usePostDataToFetchResult(finalData, setResult, result);
+
 	return (
 		<React.Fragment>
-			<Wrapper></Wrapper>
+			<SelectedPlanetWrapper>
+				<Heading color="#FAD107" fontSize="1.2rem" fontFamily="Avenir">
+					result
+				</Heading>
+			</SelectedPlanetWrapper>
 		</React.Fragment>
 	);
 };
